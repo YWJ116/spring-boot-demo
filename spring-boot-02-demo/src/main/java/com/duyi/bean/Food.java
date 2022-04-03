945dd848ccad01eeb34317091a23eb49ff1d406e
Administrator <976830634@qq.com>
2022-04-03 13:07:44 +0800

    第三次提交

--- spring-boot-02-demo/src/main/java/com/duyi/bean/Food.java
+++ spring-boot-02-demo/src/main/java/com/duyi/bean/Food.java
@@ -1,8 +1,7 @@
 package com.duyi.bean;
 
 import lombok.*;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+
 
 @Getter@Setter
 @AllArgsConstructor
---
+++ spring-boot-04-web-restful/src/test/java/com/duyi/SpringBoot04WebRestfulApplicationTests.java
@@ -0,0 +1,14 @@
+package com.duyi;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot04WebRestfulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
